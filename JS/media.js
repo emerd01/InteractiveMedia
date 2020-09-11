cfc1201fefe5434369b4a1fce842a6bbad606cec
emerd01 <64674100+emerd01@users.noreply.github.com>
2020-09-11 16:52:38 -0400

    changing format of hover

--- JS/media.js
+++ JS/media.js
@@ -8,7 +8,7 @@ d3.select("#animals")
 var setBannerText = function()
 {
     d3.select("#banner")
-    .text("Click An Image For A Description")
+    .text("Hover over an image for a description")
 }
 
 d3.select("#cat")
@@ -17,15 +17,18 @@ d3.select("#cat")
     d3.select("#banner")
     .text("His Royal Highness of Wales")
 })
+.on("mouseleave", setBannerText)
 d3.select("#panda")
     .on("mouseenter",function()
 {
     d3.select("#banner")
     .text("Sir Rothembaum having his mid-afternoon snack")
 })
+.on("mouseleave", setBannerText)
 d3.select("#flamingo")
     .on("mouseenter",function()
 {
     d3.select("#banner")
     .text("A Flock of the Bourgeoisie")
 })
+.on("mouseleave", setBannerText)

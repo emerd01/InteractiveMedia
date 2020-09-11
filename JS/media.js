5d2edf6200c62a09a49a748718f970847cf59118
emerd01 <64674100+emerd01@users.noreply.github.com>
2020-09-11 16:50:24 -0400

    added hover over images

--- JS/media.js
+++ JS/media.js
@@ -4,20 +4,27 @@ d3.select("#Smithsonian")
 d3.select("#animals")
     .classed("hidden", false);
 })
+
+var setBannerText = function()
+{
+    d3.select("#banner")
+    .text("Click An Image For A Description")
+}
+
 d3.select("#cat")
-    .on("click",function()
+    .on("mouseenter",function()
 {
     d3.select("#banner")
     .text("His Royal Highness of Wales")
 })
 d3.select("#panda")
-    .on("click",function()
+    .on("mouseenter",function()
 {
     d3.select("#banner")
     .text("Sir Rothembaum having his mid-afternoon snack")
 })
 d3.select("#flamingo")
-    .on("click",function()
+    .on("mouseenter",function()
 {
     d3.select("#banner")
     .text("A Flock of the Bourgeoisie")
